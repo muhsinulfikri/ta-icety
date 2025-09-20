b66a14002e6f7bdeb4a78bf7327c447f57195148
muhsinulfikri <89565474+muhsinulfikri@users.noreply.github.com>
2025-09-20 22:45:32 +0700

    fix app provider https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::defaultView('custom-pagination');
+        if (config('app.env') === 'production' || config('app.env') === 'development') {
+            URL::forceScheme('https');
+        }
     }
 }
